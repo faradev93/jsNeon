6b04d0387170b85756588781b178445a221eb49e
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2025-03-23 14:51:08 +0330

    install router Vite & Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router";
+import Dwork1 from "./components/Dwork1";
+
 const App = () => {
   return (
     <div>
       <div className="p-4 text-4xl text-amber-300 border-red-500/70 border-4 w-[300px] rounded-4xl text-center">
         salam
       </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dwork1" element={<Dwork1 />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/components/Dwork1.jsx
@@ -0,0 +1,8 @@
+//
+console.log("salam");
+//
+const Dwork1 = (e) => {
+  return <div>hey</div>;
+};
+
+export default Dwork1;
