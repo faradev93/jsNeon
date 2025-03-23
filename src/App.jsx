7b8da0585dd87077cc57e7a0907d702f9074a1a6
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2025-03-23 14:51:04 +0330

    install tailwind-routers & vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <div className="p-4 text-4xl text-amber-300 border-red-500/70 border-4 w-[300px] rounded-4xl text-center">
+        salam
+      </div>
+    </div>
+  );
+};
+
+export default App;
